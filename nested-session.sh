acff8afcbb6510e2a0cd776c944fcc27dd06aebe
xxanqw <potik217@gmail.com>
2026-02-20 16:42:38 +0200

    remove nested-session.sh due to --nested discontinuation

--- nested-session.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run a nested Gnome Shell session for testing the extension
-
-if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-    dbus-run-session -- gnome-shell --nested --wayland
-else
-    echo "This script only supports Wayland sessions."
-    exit 1
-fi
